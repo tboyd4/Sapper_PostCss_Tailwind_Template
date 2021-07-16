7928bdc52af2e0bcb3cc09740088680cbf48b29a
Tyler Boyd <tboyd.evan@gmail.com>
2021-07-15 22:01:12 -0500

    inital commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<main>
+  <slot />
+</main>
+
+<style global lang="postcss">
+  @import '../assets/global.pcss';
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<p>
+  <strong>A Home Page</strong>
+</p>
+
+<style lang="postcss">
+  p {
+    @apply text-center my-0 mx-auto;
+  }
+
+  p {
+    @apply my-0 mx-auto;
+  }
+
+  @screen md {
+    h1 {
+      @apply text-5xl;
+    }
+  }
+</style>
